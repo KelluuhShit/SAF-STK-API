a57dde9428951369ca364b28da7e3c1c2b603b45
KelluuhShit <kronoh60@gmail.com>
2024-10-18 15:05:24 +0300

    fully working app

---
+++ app/api/mpesa/callback.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  if (!data.Body.stkCallback.CallbackMetadata) {
+    // For failed transactions
+    console.log(data.Body.stkCallback.ResultDesc);
+    return NextResponse.json("ok saf");
+  }
+
+  // Let's extract the values from the callback metadata
+  const body = data.Body.stkCallback.CallbackMetadata;
+
+  // Use the defined type instead of 'any' here
+  const amountObj = body.Item.find(
+    (obj: { Name: string; Value: string | number }) => obj.Name === "Amount"
+  );
+  const amount = amountObj?.Value;
+
+  // Mpesa code
+  const codeObj = body.Item.find(
+    (obj: { Name: string; Value: string | number }) =>
+      obj.Name === "MpesaReceiptNumber"
+  );
+  const mpesaCode = codeObj?.Value;
+
+  // Phone number (in recent implementations, it may be hashed)
+  const phoneNumberObj = body.Item.find(
+    (obj: { Name: string; Value: string | number }) => obj.Name === "PhoneNumber"
+  );
+  const phoneNumber = phoneNumberObj?.Value?.toString();
+
+  try {
+    // Complete your logic - e.g., saving transaction to the DB
+    console.log({ amount, mpesaCode, phoneNumber });
+
+    return NextResponse.json("ok", { status: 200 });
+  } catch {
+    // Removed the unused `error` variable
+    return NextResponse.json("error", { status: 500 });
+  }
+}
---
+++ components/PaymentForm.tsx
@@ -0,0 +1,201 @@
+"use client";
+import React, { useState } from "react";
+import { AxiosError } from "axios";
+import { sendStkPush } from "../actions/stkPush";
+import { stkPushQuery } from "../actions/stkPushQuery";
+
+import PaymentSuccess from "./Success";
+import STKPushQueryLoading from "./StkQueryLoading";
+
+interface dataFromForm {
+  mpesa_phone: string;
+  name: string;
+  amount: number;
+}
+
+const kenyanPhoneNumberRegex =
+  /^(07\d{8}|01\d{8}|2547\d{8}|2541\d{8}|\+2547\d{8}|\+2541\d{8})$/;
+
+function PaymentForm() {
+  const [dataFromForm, setDataFromForm] = useState<dataFromForm>({
+    mpesa_phone: "",
+    name: "",
+    amount: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [stkQueryLoading, setStkQueryLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const stkPushQueryWithIntervals = (CheckoutRequestID: string) => {
+    let reqcount = 0;
+    const timer = setInterval(async () => {
+      reqcount += 1;
+
+      if (reqcount === 15) {
+        // handle long payment
+        clearInterval(timer);
+        setStkQueryLoading(false);
+        setLoading(false);
+        setErrorMessage("You took too long to pay");
+      }
+
+      const { data, error } = await stkPushQuery(CheckoutRequestID);
+
+      if (error) {
+        if (error instanceof AxiosError) {
+            // Now TypeScript knows that error is an AxiosError
+            if (error.response && error.response.data.errorCode !== "500.001.1001") {
+                setStkQueryLoading(false);
+                setLoading(false);
+                setErrorMessage(error.response.data.errorMessage);
+            }
+        } else {
+            // Handle other types of errors if needed
+            setStkQueryLoading(false);
+            setLoading(false);
+            setErrorMessage("An unknown error occurred."); // Or any other default message
+        }
+    }
+
+      if (data) {
+        if (data.ResultCode === "0") {
+          clearInterval(timer);
+          setStkQueryLoading(false);
+          setLoading(false);
+          setSuccess(true);
+        } else {
+          clearInterval(timer);
+          setStkQueryLoading(false);
+          setLoading(false);
+          setErrorMessage(data?.ResultDesc);
+        }
+      }
+    }, 2000);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+  
+    if (!kenyanPhoneNumberRegex.test(dataFromForm.mpesa_phone)) {
+      setLoading(false);
+      return alert("Invalid Mpesa number");
+    }
+  
+    try {
+      const { data: stkData, error: stkError } = await sendStkPush({
+        mpesa_phone: dataFromForm.mpesa_phone, // Correctly pass the new property
+        name: dataFromForm.name,
+        amount: dataFromForm.amount,
+      });
+      if (stkError) {
+        setLoading(false);
+        return alert(stkError); // Consider using a UI message instead
+      }
+  
+      const checkoutRequestId = stkData.CheckoutRequestID;
+      alert("STK Push sent successfully");
+      setStkQueryLoading(true);
+      stkPushQueryWithIntervals(checkoutRequestId);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error in sending STK Push:", error); // Consider more user-friendly error handling
+    }
+  };
+  
+
+  return (
+    <div className="lg:pl-12">
+      <div className="overflow-hidden rounded-md bg-white">
+        <div className="p-6 sm:p-10">
+          <p className="mt-4 text-base text-gray-600">
+            Provide your name, Mpesa number, and amount to process donation.
+          </p>
+          <form onSubmit={handleSubmit} className="mt-4">
+            <div className="space-y-6">
+              <div>
+                <label className="text-base font-medium text-gray-900">Name</label>
+                <div className="relative mt-2.5">
+                  <input
+                    type="text"
+                    required
+                    name="name"
+                    value={dataFromForm.name}
+                    onChange={(e) =>
+                      setDataFromForm({
+                        ...dataFromForm,
+                        name: e.target.value,
+                      })
+                    }
+                    placeholder="John Doe"
+                    className="block w-full rounded-md border border-gray-200 bg-white px-4 py-4 text-black placeholder-gray-500 caret-orange-500 transition-all duration-200 focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="text-base font-medium text-gray-900">
+                  Mpesa Number
+                </label>
+                <div className="relative mt-2.5">
+                  <input
+                    type="text"
+                    name="mpesa_phone"
+                    value={dataFromForm.mpesa_phone}
+                    onChange={(e) =>
+                      setDataFromForm({
+                        ...dataFromForm,
+                        mpesa_phone: e.target.value,
+                      })
+                    }
+                    placeholder="Enter Mpesa phone number"
+                    className="block w-full rounded-md border border-gray-200 bg-white px-4 py-4 text-black placeholder-gray-500 caret-orange-500 transition-all duration-200 focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="text-base font-medium text-gray-900">Amount</label>
+                <div className="relative mt-2.5">
+                  <input
+                    type="number"
+                    required
+                    name="amount"
+                    value={dataFromForm.amount}
+                    onChange={(e) =>
+                      setDataFromForm({
+                        ...dataFromForm,
+                        amount: Number(e.target.value),
+                      })
+                    }
+                    placeholder="Enter amount"
+                    className="block w-full rounded-md border border-gray-200 bg-white px-4 py-4 text-black placeholder-gray-500 caret-orange-500 transition-all duration-200 focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-orange-500 px-4 py-4 text-base font-semibold text-white transition-all duration-200 hover:bg-orange-600 focus:bg-orange-600 focus:outline-none"
+                >
+                  {loading ? "Processing.." : "Proceed With Payment"}
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <>
+        {stkQueryLoading ? (
+          <STKPushQueryLoading number={dataFromForm.mpesa_phone} />
+        ) : success ? (
+          <PaymentSuccess />
+        ) : errorMessage ? (
+          <div className="lg:pl-12">{errorMessage}</div>
+        ) : null}
+      </>
+    </div>
+  );
+}
+
+export default PaymentForm;
